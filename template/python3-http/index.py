59f4f81f729795ac992f80ec28234b4226fce995
ZiyunZhao <zhaoziyun@berkeley.edu>
2020-03-19 16:52:18 -0700

    my index.py work on local

--- template/python3-http/index.py
+++ template/python3-http/index.py
@@ -8,10 +8,9 @@ import sys
 from waitress import serve
 
 
-
+@app.route('/')
 #@app.route('/', defaults={'path': ''}, methods=['GET', 'PUT', 'POST', 'PATCH', 'DELETE'])
 #@app.route('/<path:path>', methods=['GET', 'PUT', 'POST', 'PATCH', 'DELETE'])
-@app.route('/')
 def handle():
     command = "cd nmt && python -m nmt.nmt \
     --out_dir=./nmt/tmp/nmt_model \
