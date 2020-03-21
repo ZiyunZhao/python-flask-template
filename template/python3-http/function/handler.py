04bd0d9e260e7f6500b37d3f3e5a6f716522e659
ZiyunZhao <zhaoziyun@berkeley.edu>
2020-03-20 18:18:16 -0700

    handler.py works

--- template/python3-http/function/handler.py
+++ template/python3-http/function/handler.py
@@ -1,12 +1,26 @@
 from nmt.nmt import nmt
+import os
+
 
 def translate():
-   return "tranlated"
-
+    command = "cd nmt && python -m nmt.nmt \
+    --out_dir=./nmt/tmp/nmt_model \
+    --inference_input_file=./nmt/tmp/my_infer_file.vi \
+    --inference_output_file=./nmt/tmp/nmt_model/output_infer && cd .."
+    f = open("./nmt/nmt/tmp/my_infer_file.vi", "w")
+    f.write("2")
+    f.close()
+    os.system(command)
+    f = open("./nmt/nmt/tmp/nmt_model/output_infer", "r")
+    content = f.read()
+    f.close()
+    return content
 
 
 def handle(event, context):
+    content = translate()
     return {
         "statusCode": 200,
-        "body": translate()
+        "body": content
     }
+
